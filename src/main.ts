f6f9330d5b0c287591ba01595f7ddb500dc7d9f8
Dave Stewart <dave@davestewart.co.uk>
2020-03-15 01:00:26 +0000

    Add fonts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
+import './assets/styles/index.scss';
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.svg">
+    <img alt="Vue logo" src="../assets/images/logo.svg">
     <HelloWorld />
   </div>
 </template>
