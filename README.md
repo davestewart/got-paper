f648bf32ce47d133c72a717f0b2498dfd708d8c3
Dave Stewart <dave@davestewart.co.uk>
2020-03-15 00:19:05 +0000

    Initial Vue PWA and assets

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# got-paper
-The official "got paper?" app
+# Got Paper ?
+
+> The official 'got paper?' app
+
+## Intro
+
+Joke app to help people calculate the right amount of toilet paper.
+
+
+## Project setup
+
+Install
+
+```
+npm install
+```
+
+Compile and hot-reload for development
+
+```
+npm run serve
+```
+
+Compile and minify for production
+
+```
+npm run build
+```
+
+Lint and fix files
+
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  background-color: #1660A6;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #eaeaea;
+}
+
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #000000;
+    &.router-link-exact-active {
+      color: #FFFFFF;
+    }
+  }
+}
+</style>
