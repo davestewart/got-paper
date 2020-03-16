9398822e1929d3ec23651ff4cd484a52bb8b62d8
Dave Stewart <dave@davestewart.co.uk>
2020-03-16 12:05:17 +0000

    Fix form reset after submit

--- src/views/pages/Contact.vue
+++ src/views/pages/Contact.vue
@@ -2,49 +2,17 @@
   <div class="about">
     <h1>Contact</h1>
 
-    <ValidationObserver v-slot="{ invalid }">
+    <ValidationObserver ref="observer" v-slot="{ invalid, handleSubmit }">
       <form name="contact"
             method="post"
             data-netlify="true"
             data-x-netlify-recaptcha="true"
             data-netlify-honeypot="bot-field"
-            @submit.prevent="submit">
+            @submit.prevent="handleSubmit(submit)">
         <!-- name -->
         <input type="hidden" name="form-name" value="contact"/>
 
-        <!-- Name input-->
-        <div class="form-group row">
-          <label class="col-md-3 control-label" for="name">Name</label>
-          <div class="col-md-9">
-            <ValidationProvider :rules="rules.name" v-slot="v">
-              <input id="name" v-model="form.name" name="name" type="text" placeholder="Your name" class="form-control">
-              <small class="form-text text-danger ml-2">{{ v.errors[0] }}</small>
-            </ValidationProvider>
-          </div>
-        </div>
-
-        <!-- Email input-->
-        <div class="form-group row">
-          <label class="col-md-3 control-label" for="email">E-mail</label>
-          <div class="col-md-9">
-            <ValidationProvider :rules="rules.email" v-slot="v">
-              <input id="email" v-model="form.email" name="email" type="text" placeholder="Your email" class="form-control">
-              <small class="form-text text-danger ml-2">{{ v.errors[0] }}</small>
-            </ValidationProvider>
-          </div>
-        </div>
-
-        <!-- Message body -->
-        <div class="form-group row">
-          <label class="col-md-3 control-label" for="message">Message</label>
-          <div class="col-md-9">
-            <ValidationProvider :rules="rules.message" v-slot="v">
-              <textarea class="form-control" id="message" v-model="form.message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
-              <small class="form-text text-danger ml-2">{{ v.errors[0] }}</small>
-            </ValidationProvider>
-          </div>
-        </div>
-
+        <!-- alert -->
         <div class="form-group row mb-0">
           <div class="offset-md-3 col-md-9 mb-0">
             <div v-if="result.message" class="alert alert-dismissible fade show" role="alert" :class="`alert-${result.state}`">
@@ -56,10 +24,44 @@
           </div>
         </div>
 
-        <!-- Form actions -->
+        <!-- name input-->
+        <div class="form-group row">
+          <label class="col-md-3 control-label" for="name">Name</label>
+          <div class="col-md-9">
+            <ValidationProvider :rules="rules.name" v-slot="v">
+              <input id="name" v-model="form.name" name="name" type="text" placeholder="Your name" class="form-control">
+              <small class="form-text text-danger ml-2">{{ v.errors[0] }}</small>
+            </ValidationProvider>
+          </div>
+        </div>
+
+        <!-- email input-->
+        <div class="form-group row">
+          <label class="col-md-3 control-label" for="email">E-mail</label>
+          <div class="col-md-9">
+            <ValidationProvider :rules="rules.email" v-slot="v">
+              <input id="email" v-model="form.email" name="email" type="text" placeholder="Your email" class="form-control">
+              <small class="form-text text-danger ml-2">{{ v.errors[0] }}</small>
+            </ValidationProvider>
+          </div>
+        </div>
+
+        <!-- message body -->
+        <div class="form-group row">
+          <label class="col-md-3 control-label" for="message">Message</label>
+          <div class="col-md-9">
+            <ValidationProvider :rules="rules.message" v-slot="v">
+              <textarea class="form-control" id="message" v-model="form.message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+              <small class="form-text text-danger ml-2">{{ v.errors[0] }}</small>
+            </ValidationProvider>
+          </div>
+        </div>
+
+        <!-- form actions -->
         <div class="form-group row">
           <div class="offset-md-3 col-9">
             <button type="submit" class="btn btn-primary" :disabled="invalid">Submit</button>
+            <button class="btn ml-2" @click.prevent="reset">Reset</button>
           </div>
         </div>
       </form>
@@ -132,6 +134,7 @@ export default {
 
     reset () {
       this.form = reset()
+      this.$refs.observer.reset()
     },
 
     showResult (message = '', state = '') {
