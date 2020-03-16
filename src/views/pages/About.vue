edd05807ca1500c1478daf8e9ac5a0b928a675d8
Dave Stewart <dave@davestewart.co.uk>
2020-03-16 20:36:50 +0000

    Update About text

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="about">
     <h1>Hello</h1>
-    <p>When I told friend I was going to make this tool and call it "Got Paper" he said:</p>
-    <div class="ml-4">
-      <p>"It's very catchy"</p>
-      <p>"Sure" I replied...</p>
-      <p>"A bit like Coronavirus"</p>
-    </div>
-    <p><strong>Got Paper</strong> exists for several reasons:</p>
-    <ul>
-      <li>To be a useful resource</li>
-      <li>To show up the hoarding for what it is</li>
-      <li>To raise a laugh or two</li>
-    </ul>
-    <p>If you find it useful, or funny, please share. If you're press, here's a <a href="https://drive.google.com/drive/folders/1lA8-GC8pBUxrrSKOcnp7OnJ-UPHx-952?usp=sharing" target="_blank">Press Kit</a>.</p>
-    <p><strong>Thank you</strong></p>
-    <p><a href="http://twitter.com/dave_stewart">@dave_stewart</a></p>
+    <section>
+      <p>When I told friend I was going to make this tool and call it "Got Paper" he said:</p>
+      <div class="ml-4">
+        <p class="mb-2">"It's very catchy"</p>
+        <p class="mb-2">"Sure" I replied...</p>
+        <p class="mb-2">"A bit like Coronavirus"</p>
+      </div>
+    </section>
+
+    <section>
+      <p><strong>Got Paper</strong> exists for several reasons:</p>
+      <ul>
+        <li>To be a useful resource</li>
+        <li>To show up the hoarding for what it is</li>
+        <li>To raise a laugh or two</li>
+      </ul>
+      <p>If you find it useful, or funny, please share.</p>
+    </section>
+
+    <section>
+      <p><strong>If you're a journalist, here's a <a href="https://drive.google.com/drive/folders/1lA8-GC8pBUxrrSKOcnp7OnJ-UPHx-952?usp=sharing" target="_blank">Press Kit</a>.</strong></p>
+    </section>
+
+    <section>
+      <p>Thank you</p>
+      <p><a href="http://twitter.com/dave_stewart">@dave_stewart</a></p>
+    </section>
+
   </div>
 </template>
+
+<style>
+.about section {
+  margin: 1.5rem 0;
+}
+</style>
