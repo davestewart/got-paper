f082f7b07aecb776af4d250e87dab7797ef2cb40
Dave Stewart <dave@davestewart.co.uk>
2020-03-16 19:23:59 +0000

    Update About with twitter and press kit

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1>Hello</h1>
-    <p>When I suggested to a friend to make site about toilet paper called "Got Paper" he said:</p>
-    <div class="4">
+    <p>When I told friend I was going to make a this tool and call it "Got Paper" he said:</p>
+    <div class="ml-4">
       <p>"It's very catchy"</p>
       <p>"Sure" I replied...</p>
       <p>"A bit like Coronavirus"</p>
@@ -10,10 +10,11 @@
     <p><strong>Got Paper</strong> exists for several reasons:</p>
     <ul>
       <li>To be a useful resource</li>
-      <li>To ridicule the hoarders</li>
+      <li>To put the hoarding in perspective</li>
       <li>To raise a laugh or two</li>
     </ul>
-    <p>If you find it useful, or funny, please share.</p>
+    <p>If you find it useful, or funny, please share. If you're press, here's a <a href="https://drive.google.com/drive/folders/1lA8-GC8pBUxrrSKOcnp7OnJ-UPHx-952?usp=sharing" target="_blank">Press Kit</a>.</p>
     <p><strong>Thank you</strong></p>
+    <p><a href="http://twitter.com/dave_stewart">@dave_stewart</a></p>
   </div>
 </template>
