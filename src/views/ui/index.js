8653ae744a0cbf41ae78c6a9f4b75bca520897d2
Dave Stewart <dave@davestewart.co.uk>
2020-03-18 13:14:18 +0000

    Add UiAlert component

--- src/views/ui/index.js
+++ src/views/ui/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import UiIcon from './UiIcon'
 import UiNumber from './UiNumber.vue'
+import UiAlert from './UiAlert.vue'
 import UiOutput from './UiOutput.vue'
 import UiIconButton from './UiIconButton.vue'
 import UiContainer from './UiContainer.vue'
 
 Vue.component('UiIcon', UiIcon)
 Vue.component('UiNumber', UiNumber)
+Vue.component('UiAlert', UiAlert)
 Vue.component('UiOutput', UiOutput)
 Vue.component('UiIconButton', UiIconButton)
 Vue.component('UiContainer', UiContainer)
