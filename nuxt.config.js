65c77008d49cadc39b20b21d7eea0175d2324802
Alexander Lichter <github@lichter.io>
2020-03-21 11:34:06 +0100

    chore: use pwa-icon generator and add more head attrs

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,42 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: c => c ? `${c} - Got Paper ` : 'Got Paper - Calculate your actual toilet paper needs',
+    titleTemplate: c => c ? `${c} - Got Paper?` : 'Got Paper? - Calculate your actual toilet paper needs',
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    meta: [
+      {
+        hid: 'twitter:image:src',
+        name: 'twitter:image:src',
+        property: 'twitter:image:src',
+        content: `${BASE_URL}/images/social/splash-twitter.png`
+      },
+      {
+        hid: 'sailthru.image.full',
+        name: 'sailthru.image.full',
+        property: 'sailthru.image.full',
+        content: `${BASE_URL}/images/social/splash-whatsapp.png`
+      },
+      {
+        hid: 'sailthru.image.thumb',
+        name: 'sailthru.image.thumb',
+        property: 'sailthru.image.thumb',
+        content: `${BASE_URL}/images/social/splash-whatsapp-small.png`
+      }
     ]
   },
   meta: {
-    description
+    ogSiteName: 'Got Paper?',
+    ogTitle: 'Calculate your actual toilet paper needs',
+    description,
+    ogHost: BASE_URL,
+    ogImage: '/images/social/splash-og',
+    author: 'Dave Stewart',
+    twitterCard: 'summary_large_image',
+    twitterSite: '@haveyougotpaper',
+    theme_color: '#1660A6',
+    background_color: '#1660A6'
   },
   /*
   ** Customize the progress-bar color
