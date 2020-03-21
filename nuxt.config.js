62578205ab4b708cee4c01ba8e303d4e8ea23871
Dave Stewart <dave@davestewart.co.uk>
2020-03-21 23:50:15 +0000

    Add all app/pwa icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 export const site = {
-  url: 'https://got-paper.com',
+  url: isProd ? 'https://got-paper.com' : 'http://localhost:3000',
   name: 'Got Paper?',
   title: 'Calculate your actual toilet paper needs',
   shortDescription: 'A cheeky online app to calculate your toilet paper needs during the Coronavirus pandemic',
   description: 'A cheeky desktop, tablet and mobile app to calculate you and your family\'s toilet paper needs during the Coronavirus pandemic',
   hashtags: 'gotpaper,coronavirus'
-}
-
-function image (path) {
-  return `${site.url}/images/${path.replace(/^\//, '')}`
 }
 
 function meta (property, content, hid = true) {
@@ -21,26 +19,95 @@ function meta (property, content, hid = true) {
 }
 
 function link (rel, href, data = {}) {
-  return { rel, href, ...data}
+  return { rel, href, ...data }
 }
 
 function script (src) {
   return { src }
 }
 
+function image (path) {
+  return `${site.url}/images/${path.replace(/^\//, '')}`
+}
+
+function icon (src) {
+  const matches = src.match(/(\d+x\d+).*?\.(\w+)$/)
+  const data = { src: `/images/icons/${src}`, sizes: matches[1], type: `image/${matches[2]}` }
+  if (src.includes('masked')) {
+    data.purpose = 'maskable'
+  }
+  return data
+}
+
+function favicon (type, px) {
+  return link(type, `/images/icons/${type}-${px}x${px}.png`, { sizes: `${px}x${px}` })
+}
+
+const pwa = {
+  manifest: {
+    name: site.name,
+    short_name: site.name,
+    start_url: '/',
+    scope: '/',
+    display: 'standalone',
+    theme_color: '#1660A6',
+    background_color: '#1660A6',
+    icons: [
+      // android icons
+      icon('android-chrome-192x192.png'),
+      icon('android-chrome-512x512.png'),
+      icon('android-chrome-192x192-masked.png'),
+      icon('android-chrome-512x512-masked.png'),
+    ]
+  },
+  icon: {
+    sizes: []
+  }
+}
+
 export default {
   mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // server
+
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+
+  netlifyFiles: {
+    existingFilesDirectory: './netlify' // The directory where your _headers and _redirects file is located
+  },
+
+  pwa,
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // app
+
+  axios: { baseURL: site.url },
+
+  router: {
+    trailingSlashes: false
+  },
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // styles
+
   head: {
     titleTemplate: c => c ? `${c} - Got Paper?` : 'Got Paper? - Calculate your actual toilet paper needs',
     link: [
-      link('icon', '/favicon.ico', { type: 'image/x-icon' }),
-      link('icon', image('/social/icon-whatsapp-small.png'), { sizes: '192x192' }),
+      // icons
+      link('icon', '/favicon.png'),
+      link('mask-icon', image('/icons/safari-pinned-tab.svg', { color: '#4DBA87' })),
+      ...[180, 152, 120, 76, 60].map(px => favicon('apple-touch-icon', px)),
+      ...[256, 32, 16].map(px => favicon('favicon', px)),
+
+      // assets
       link('stylesheet', 'https://use.typekit.net/lvl2shj.css'),
       link('stylesheet', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css')
     ],
+
     meta: [
       // google
       meta('name', site.name),
@@ -50,14 +117,16 @@ export default {
       // twitter
       meta('twitter:image:src', image('/social/splash-twitter.png')),
 
-      // linked in
-      meta('sailthru.image.full', image('/social/icon-whatsapp.png')),
-      meta('sailthru.image.thumb', image('/social/icon-whatsapp-small.png'))
+      // windows
+      meta('msapplication-TileImage', image('/icons/msapplication-icon-144x144.png')),
+      meta('msapplication-TileColor', '#000000'),
     ],
+
     script: [
       script('https://ko-fi.com/widgets/widget_2.js')
     ]
   },
+
   meta: {
     ogSiteName: site.name,
     ogTitle: site.title,
@@ -71,52 +140,42 @@ export default {
     background_color: '#1660A6',
     mobileAppIOS: true
   },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // styles
+
   css: [
     '@/assets/styles/index'
   ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/polyfills.client.js',
-    '@/plugins/global-components.js'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxt/typescript-build',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/netlify-files'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa'
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-    baseURL: site.url
-  },
+
   styleResources: {
     scss: [
       '@/assets/styles/_variables.scss'
     ]
   },
-  netlifyFiles: {
-    existingFilesDirectory: './netlify' // The directory where your _headers and _redirects file is located
+
+  // ---------------------------------------------------------------------------------------------------------------------
+  // framework
+
+  plugins: [
+    '@/plugins/polyfills.client.js',
+    '@/plugins/global-components.js'
+  ],
+
+  buildModules: [
+    // https://nuxtjs.org/api/configuration-modules
+    '@nuxt/typescript-build',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/netlify-files'
+  ],
+
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
+  ],
+
+  loading: {
+    color: '#fff'
   }
 }
