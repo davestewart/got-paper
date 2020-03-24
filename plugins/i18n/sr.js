d716893fc32f91358b313e847274a2176337b126
Dave Stewart <dave@davestewart.co.uk>
2020-03-24 21:36:32 +0000

    Clarify Serbian choice function

--- plugins/i18n/sr.js
+++ plugins/i18n/sr.js
@@ -1,13 +1,13 @@
-export function srChoiceIndex (choice, choicesLength) {
+export function srChoiceIndex (value, choicesLength) {
   // '0 rolni | {n} rolnu | {n} rolne'
 
   // values
-  choice = Math.floor(choice)
-  const v10 = choice % 10
-  const v100 = choice % 100
+  const v = Math.floor(value)
+  const v10 = v % 10
+  const v100 = v % 100
 
   // pattern
-  const isNone = choice === 0
+  const isNone = v === 0
   const endsIn1 = v10 === 1
   const isFew = v10 >= 2 && v10 <= 4
   const isTeen = v100 > 10 && v100 < 20
