f213c55927ccf8e02e5696e1caa2721cc00d0b71
Dave Stewart <dave@davestewart.co.uk>
2020-04-01 16:35:14 +0100

    Add updated proportional resize package

--- plugins/proportional-resize.js
+++ plugins/proportional-resize.js
@@ -1,34 +1,4 @@
 import Vue from 'vue'
-
-/**
- * Code forked from https://github.com/MarxJiao/vue-proportion-directive
- */
-const proportion = {
-  install (Vue) {
-    function update (el, binding) {
-      const w = el.offsetWidth
-      const h = w * binding.value
-      el.style.height = h + 'px'
-    }
-
-    Vue.directive('proportion', {
-      inserted (el, binding) {
-        const w = el.offsetWidth
-        const h = w * binding.value
-        el.style.height = h + 'px'
-        el.__resize = () => update(el, binding)
-        window.addEventListener('resize', el.__resize)
-      },
-
-      componentUpdated (el, binding) {
-        update(el, binding)
-      },
-
-      unbind (el) {
-        window.removeEventListener('resize', el.__resize)
-      }
-    })
-  }
-}
+import proportion from 'vue-proportion-directive'
 
 Vue.use(proportion)
